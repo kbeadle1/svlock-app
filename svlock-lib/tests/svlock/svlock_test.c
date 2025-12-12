8a061982c62748a9bbe2a905a50728c48cc7d7f4
Kevin Beadle <kevin.beadle@intel.com>
2025-12-12 09:57:51 -0800

    add svlock tests

--- svlock-lib/tests/svlock/svlock_test.c
+++ svlock-lib/tests/svlock/svlock_test.c
@@ -1,9 +1,67 @@
-#include "config.h"
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include "svlock/svlock.h"
 
 int main(int argc, char *argv[])
 {
+    int res = 0;
+
+    res = svlock_init_index(0, 1);
+    if (res != 0) {
+        printf("Failed svlock_init_index\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_init_index\n");
+
+    res = svlock_get_initialized(0);
+    if (res != 1) {
+        printf("Failed svlock_get_initialized\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_get_initialized\n");
+    
+    res = svlock_getvalue(0);
+    if (res != 1) {
+        printf("Failed svlock_getvalue\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_getvalue\n");
+    
+    res = svlock_acquire(0);
+    if (res != 0) {
+        printf("Failed svlock_acquire\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_acquire\n");
+    
+    res = svlock_getvalue(0);
+    if (res != 0) {
+        printf("Failed svlock_getvalue\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_getvalue\n");
+    
+    res = svlock_release(0);
+    if (res != 0) {
+        printf("Failed svlock_release\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_release\n");
+    
+    res = svlock_getvalue(0);
+    if (res != 1) {
+        printf("Failed svlock_getvalue\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_getvalue\n");
+    
+    res = svlock_cleanup();
+    if (res != 0) {
+        printf("Failed svlock_cleanup\n");
+        return EXIT_FAILURE;
+    }
+    printf("Passed svlock_cleanup\n");
+
     return 0;
 }
--- svlock-lib/tests/svlock/touch-svlock-apis.py
+++ svlock-lib/tests/svlock/touch-svlock-apis.py
@@ -1,8 +1,52 @@
 #!/usr/bin/python3
 
+import sys
 from svlock import *
 
-res = svlock_is_initialized(0)
-
+res = svlock_init_index(0, 1)
 if res != 0:
-	print("error: svlock_is_initialized() failed")
+    print("Failed svlock_init_index")
+    sys.exit(1)
+print("Passed svlock_init_index")
+    
+res = svlock_get_initialized(0)
+if res != 1:
+    print("Failed svlock_get_initialized")
+    sys.exit(1)
+print("Passed svlock_get_initialized")
+    
+res = svlock_getvalue(0)
+if res != 1:
+    print("Failed svlock_getvalue")
+    sys.exit(1);
+print("Passed svlock_getvalue")
+    
+res = svlock_acquire(0)
+if res != 0:
+    print("Failed svlock_acquire")
+    sys.exit(1)
+print("Passed svlock_acquire");
+
+res = svlock_getvalue(0)
+if res != 0:
+    print("Failed svlock_getvalue")
+    sys.exit(1)
+print("Passed svlock_getvalue")
+
+res = svlock_release(0)
+if res != 0:
+    print("Failed svlock_release")
+    sys.exit(1)
+print("Passed svlock_release")
+
+res = svlock_getvalue(0)
+if res != 1:
+    print("Failed svlock_getvalue")
+    sys.exit(1)
+print("Passed svlock_getvalue")
+
+res = svlock_cleanup()
+if res != 0:
+    print("Failed svlock_cleanup")
+    sys.exit(1)
+print("Passed svlock_cleanup")
